67d2890cb6e39de556aa98b836204fc3f8714495
Your Name <e.oracle2000@gmail.com>
2024-12-02 14:12:20 +0200

    add custom list view item to hom

--- lib/Features/home/presintation/views/widgets/home_view_body.dart
+++ lib/Features/home/presintation/views/widgets/home_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:my_bookly_app/Features/home/presintation/views/widgets/custom_app_bar.dart';
+import 'package:my_bookly_app/Features/home/presintation/views/widgets/custom_list_view_item.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
@@ -9,6 +10,7 @@ class HomeViewBody extends StatelessWidget {
     return Column(
       children: const [
         CustomAppBar(),
+        CustomListViewItem(),
       ],
     );
   }
