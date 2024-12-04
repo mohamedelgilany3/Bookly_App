9419b00bc0ce9c51fc9468dc4179c3bcb66ea39d
Your Name <e.oracle2000@gmail.com>
2024-12-04 20:21:10 +0200

    best seller list view item part one

--- lib/Features/home/presintation/views/widgets/home_view_body.dart
+++ lib/Features/home/presintation/views/widgets/home_view_body.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:my_bookly_app/Features/home/presintation/views/widgets/custom_app_bar.dart';
 import 'package:my_bookly_app/Features/home/presintation/views/widgets/featrued_list_view.dart';
+import 'package:my_bookly_app/core/utils/assets.dart';
 import 'package:my_bookly_app/core/utils/styles.dart';
 
 class HomeViewBody extends StatelessWidget {
@@ -19,6 +20,34 @@ class HomeViewBody extends StatelessWidget {
             height: 16,
           ),
           Text('Best Seller', style: Styles.titleMedium),
+          BestSellerListViewItem(),
+        ],
+      ),
+    );
+  }
+}
+
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 130,
+      child: Row(
+        children: [
+          AspectRatio(
+            aspectRatio: 2.5 / 4,
+            child: Container(
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8),
+                  image: const DecorationImage(
+                      fit: BoxFit.fill,
+                      image: AssetImage(
+                        AssetsData.testImage,
+                      ))),
+            ),
+          ),
         ],
       ),
     );
