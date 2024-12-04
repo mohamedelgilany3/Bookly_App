75342053a4e6c34e66dcabf6e59351f851712e27
Your Name <e.oracle2000@gmail.com>
2024-12-04 13:20:56 +0200

    add featured list view to home

--- lib/Features/home/presintation/views/widgets/home_view_body.dart
+++ lib/Features/home/presintation/views/widgets/home_view_body.dart
@@ -10,8 +10,25 @@ class HomeViewBody extends StatelessWidget {
     return Column(
       children: const [
         CustomAppBar(),
-        CustomListViewItem(),
+        FeaturedBooksListView(),
       ],
+    );
+  }
+}
+
+class FeaturedBooksListView extends StatelessWidget {
+  const FeaturedBooksListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * 0.3,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, index) {
+          return const CustomListViewItem();
+        },
+      ),
     );
   }
 }
