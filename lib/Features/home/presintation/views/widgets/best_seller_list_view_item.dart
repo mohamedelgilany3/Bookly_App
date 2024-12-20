991eb1e57cc6e4abdd475d13787f04f08b99fe83
Your Name <e.oracle2000@gmail.com>
2024-12-20 16:09:11 +0200

    bast seller item

--- lib/Features/home/presintation/views/widgets/best_seller_list_view_item.dart
+++ lib/Features/home/presintation/views/widgets/best_seller_list_view_item.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:my_bookly_app/Features/home/presintation/views/widgets/book_rating.dart';
 import 'package:my_bookly_app/constant.dart';
 import 'package:my_bookly_app/core/utils/assets.dart';
 import 'package:my_bookly_app/core/utils/styles.dart';
@@ -27,35 +29,40 @@ class BestSellerListViewItem extends StatelessWidget {
           const SizedBox(
             width: 30,
           ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(
-                width: MediaQuery.of(context).size.width * .5,
-                child: Text(
-                  'Harry Potter and the goblet of fire',
-                  maxLines: 2,
-                  overflow: TextOverflow.ellipsis,
-                  style: Styles.textStyle20.copyWith(fontFamily: kGtSectraFine),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * .5,
+                  child: Text(
+                    'Harry Potter and the goblet of fire',
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                    style:
+                        Styles.textStyle20.copyWith(fontFamily: kGtSectraFine),
+                  ),
                 ),
-              ),
-              //----
-              SizedBox(height: 3),
-              Text(
-                'J.K. Rowling',
-                style: Styles.textStyle14,
-              ),
-              SizedBox(height: 3),
-              Row(
-                children: [
-                  Text(
-                    '19.99 E',
-                    style: Styles.textStyle20
-                        .copyWith(fontWeight: FontWeight.bold),
-                  )
-                ],
-              )
-            ],
+                //----
+                SizedBox(height: 3),
+                Text(
+                  'J.K. Rowling',
+                  style: Styles.textStyle14,
+                ),
+                SizedBox(height: 3),
+                Row(
+                  children: [
+                    Text(
+                      '19.99 E',
+                      style: Styles.textStyle20
+                          .copyWith(fontWeight: FontWeight.bold),
+                    ),
+                    const Spacer(),
+                    const BookRating(),
+                  ],
+                )
+              ],
+            ),
           ),
         ],
       ),
