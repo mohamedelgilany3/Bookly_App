464a0294281e65a89218f17abe03419167e25606
Your Name <e.oracle2000@gmail.com>
2024-12-29 12:26:34 +0200

    create custom book details app bar

--- lib/Features/home/presintation/views/widgets/best_seller_list_view_item.dart
+++ lib/Features/home/presintation/views/widgets/best_seller_list_view_item.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:go_router/go_router.dart';
 import 'package:my_bookly_app/Features/home/presintation/views/widgets/book_rating.dart';
 import 'package:my_bookly_app/constant.dart';
+import 'package:my_bookly_app/core/utils/app_router.dart';
 import 'package:my_bookly_app/core/utils/assets.dart';
 import 'package:my_bookly_app/core/utils/styles.dart';
 
@@ -10,61 +12,66 @@ class BestSellerListViewItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 130,
-      child: Row(
-        children: [
-          AspectRatio(
-            aspectRatio: 2.5 / 4,
-            child: Container(
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(8),
-                  image: const DecorationImage(
-                      fit: BoxFit.fill,
-                      image: AssetImage(
-                        AssetsData.testImage,
-                      ))),
+    return GestureDetector(
+      onTap: () {
+        GoRouter.of(context).push(AppRouter.kBookDetailsView);
+      },
+      child: SizedBox(
+        height: 130,
+        child: Row(
+          children: [
+            AspectRatio(
+              aspectRatio: 2.5 / 4,
+              child: Container(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8),
+                    image: const DecorationImage(
+                        fit: BoxFit.fill,
+                        image: AssetImage(
+                          AssetsData.testImage,
+                        ))),
+              ),
             ),
-          ),
-          const SizedBox(
-            width: 30,
-          ),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(
-                  width: MediaQuery.of(context).size.width * .5,
-                  child: Text(
-                    'Harry Potter and the goblet of fire',
-                    maxLines: 2,
-                    overflow: TextOverflow.ellipsis,
-                    style:
-                        Styles.textStyle20.copyWith(fontFamily: kGtSectraFine),
-                  ),
-                ),
-                //----
-                SizedBox(height: 3),
-                Text(
-                  'J.K. Rowling',
-                  style: Styles.textStyle14,
-                ),
-                SizedBox(height: 3),
-                Row(
-                  children: [
-                    Text(
-                      '19.99 E',
+            const SizedBox(
+              width: 30,
+            ),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * .5,
+                    child: Text(
+                      'Harry Potter and the goblet of fire',
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
                       style: Styles.textStyle20
-                          .copyWith(fontWeight: FontWeight.bold),
+                          .copyWith(fontFamily: kGtSectraFine),
                     ),
-                    const Spacer(),
-                    const BookRating(),
-                  ],
-                )
-              ],
+                  ),
+                  //----
+                  SizedBox(height: 3),
+                  Text(
+                    'J.K. Rowling',
+                    style: Styles.textStyle14,
+                  ),
+                  SizedBox(height: 3),
+                  Row(
+                    children: [
+                      Text(
+                        '19.99 E',
+                        style: Styles.textStyle20
+                            .copyWith(fontWeight: FontWeight.bold),
+                      ),
+                      const Spacer(),
+                      const BookRating(),
+                    ],
+                  )
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
--- lib/Features/home/presintation/views/widgets/book_details_view_body.dart
+++ lib/Features/home/presintation/views/widgets/book_details_view_body.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:my_bookly_app/Features/home/presintation/views/widgets/custom_book_details_app_bar.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
   const BookDetailsViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30),
+      child: Column(
+        children: const [
+          CustomBookDetailsAppBr(),
+        ],
+      ),
+    );
   }
 }
